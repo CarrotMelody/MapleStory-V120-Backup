b59d8bb2fa297417d45f702b7117ab9f79b8907f
Carrot M <85579546+CarrotMelody@users.noreply.github.com>
2022-06-26 00:13:11 +0800

    簡化技能學習 NPC 腳本

--- scripts/9310018 技能學習.js
+++ scripts/9310018 技能學習.js
@@ -1,10 +1,12 @@
-/*技能獲得*/
-
+/**
+ * 技能學習
+ * 2022/06/25
+ */
 var status = 0;
+var skillList = [[1004, 1], [1003, 1], [1007, 3], [8, 3]]; // [技能id, 等級]
 
 function start() {
   status = -1;
-
   action(1, 0, 0);
 }
 
@@ -13,57 +15,32 @@ function action(mode, type, selection) {
     cm.dispose();
   } else {
     if (status >= 0 && mode == 0) {
-      cm.sendOk("改天再來學吧~");
+      cm.sendOk("學而時習之，不亦說乎？");
       cm.dispose();
       return;
     }
+
     if (mode == 1) {
       status++;
     } else {
       status--;
     }
+
     if (status == 0) {
-      cm.sendSimple(
-        "#k請選擇你要學的技能： \r\n#L0##s1004##q1004##l       \r\n#L1##s1003##q1003##l    \r\n#L2##s1007##q1007##l      \r\n#L3##s8##q8##l  \r\n#L4##s4311003##q4311003##r(影武者)#k#l \r\n#L5##s4321000##q4321000##r(影武者)#b#k \r\n#L6#領取#i1902000##z1902000##l \r\n#L7#領取#i1902001##z1902001##l \r\n#L8#領取#i1912000##z1912000##l"
-      );
-    } else if (status == 1) {
-      if (selection == 0) {
-        cm.teachSkill(1004, 1, 1);
-        cm.sendOk("你已經獲得「怪物騎乘」技能！");
-        cm.dispose();
-      } else if (selection == 1) {
-        cm.teachSkill(1003, 1, 1);
-        cm.sendOk("你已經獲得「神匠之魂」技能！");
-        cm.dispose();
-      } else if (selection == 2) {
-        cm.teachSkill(1007, 3, 3);
-        cm.sendOk("你已經獲得「強化合成」技能！");
-        cm.dispose();
-      } else if (selection == 3) {
-        cm.teachSkill(8, 3, 3);
-        cm.sendOk("你已經獲得「寵物達人」技能！");
-        cm.dispose();
-      } else if (selection == 4) {
-        cm.teachSkill(4311003, 7, 20);
-        cm.sendOk("你已經獲得「狂刃風暴」技能！");
-        cm.dispose();
-      } else if (selection == 5) {
-        cm.teachSkill(4321000, 7, 20);
-        cm.sendOk("你已經獲得「雙刃旋」技能！");
-        cm.dispose();
-      } else if (selection == 6) {
-        cm.gainItem(1902000, 1);
-        cm.sendOk("你已經獲得「野豬」！");
-        cm.dispose();
-      } else if (selection == 7) {
-        cm.gainItem(1902001, 1);
-        cm.sendOk("你已經獲得「銀色鬃毛」！");
-        cm.dispose();
-      } else if (selection == 8) {
-        cm.gainItem(1912000, 1);
-        cm.sendOk("你已經獲得「馬鞍」！");
-        cm.dispose();
+      var msg = "#k請選擇你要學的技能：";
+
+      for (var i = 0; i < skillList.length; i++) {
+        msg += "\r\n#L" + i + "##s" + skillList[i][0] + "##q" + skillList[i][0] + "##l";
       }
+
+      cm.sendSimple(msg);
+    } else if (status == 1) {
+      var skill = skillList[selection][0];
+      var level = skillList[selection][1];
+
+      cm.teachSkill(skill, level, level);
+      cm.sendOk("你已經獲得「#q" + skill + "#」技能！");
+      cm.dispose();
     }
   }
-}
+} 
